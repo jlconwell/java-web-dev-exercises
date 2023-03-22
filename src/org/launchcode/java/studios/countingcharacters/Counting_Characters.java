9c0143cc52d3317996b08dddd36e7a12271ea730
Jake Conwell <jakelconwell@gmail.com>
2023-03-22 11:43:12 -0500

    completed studio for chapter 3 with bonus & super bonus

--- src/org/launchcode/java/studios/countingcharacters/Counting_Characters.java
+++ src/org/launchcode/java/studios/countingcharacters/Counting_Characters.java
@@ -3,15 +3,22 @@ package org.launchcode.java.studios.countingcharacters;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
 
 public class Counting_Characters {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+        throws IOException
+    {
 //        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a sentence:");
-        String quote = input.nextLine();
-        quote = quote.toLowerCase();
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter a sentence:");
+
+        Path filename = Path.of("C:\\Users\\jakel\\JakeConwell\\Desktop\\LC101\\studio text file.txt");
+//        String quote = input.nextLine();
+        String quote = Files.readString(filename);
 
         char[] quoteCharArray = quote.toCharArray();
         HashMap<Character, Integer> charCount = new HashMap<>();
